37851ee4014ab3b3d69daccfa182ba7d78994687
tigorsun <87004057+GELGELAT@users.noreply.github.com>
2026-09-21 16:29:25 +0300

    add asserts and fix for cmpf

---
+++ C_sort_work_lib/C_sort_work_lib_scripts/sorts/sort_asserts/sort_main_assert.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "sort_asserts.h"
+/*
+int main(void)
+{
+    int_test();
+    float_test();
+    char_test();
+    double_test();
+    string_test();
+    printf("accept\n");
+    
+}
+    */
